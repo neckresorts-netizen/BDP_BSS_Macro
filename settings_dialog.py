f4545641c7eaa2760367d7224e9c86f738bc6c0c
neckresorts-netizen <neckresorts@gmail.com>
2026-01-03 22:13:17 +0000

    Update hotkey labels and expand options in settings

--- settings_dialog.py
+++ settings_dialog.py
@@ -8,21 +8,25 @@ class SettingsDialog(QDialog):
     def __init__(self, start_key, stop_key):
         super().__init__()
         self.setWindowTitle("Settings")
-
-        self.start_key = start_key
-        self.stop_key = stop_key
+        self.setFixedSize(260, 160)
 
         layout = QVBoxLayout(self)
 
-        layout.addWidget(QLabel("Start Macro Key"))
+        layout.addWidget(QLabel("Start Macro Hotkey"))
         self.start_combo = QComboBox()
-        self.start_combo.addItems(["f5", "f7", "f8", "f9"])
+        self.start_combo.addItems([
+            "f1","f2","f3","f4","f5","f6",
+            "f7","f8","f9","f10","f11","f12"
+        ])
         self.start_combo.setCurrentText(start_key)
         layout.addWidget(self.start_combo)
 
-        layout.addWidget(QLabel("Stop Macro Key"))
+        layout.addWidget(QLabel("Stop Macro Hotkey"))
         self.stop_combo = QComboBox()
-        self.stop_combo.addItems(["f6", "f7", "f8", "f9"])
+        self.stop_combo.addItems([
+            "f1","f2","f3","f4","f5","f6",
+            "f7","f8","f9","f10","f11","f12"
+        ])
         self.stop_combo.setCurrentText(stop_key)
         layout.addWidget(self.stop_combo)
 
@@ -31,4 +35,7 @@ class SettingsDialog(QDialog):
         layout.addWidget(save_btn)
 
     def get_keys(self):
-        return self.start_combo.currentText(), self.stop_combo.currentText()
+        return (
+            self.start_combo.currentText(),
+            self.stop_combo.currentText()
+        )
